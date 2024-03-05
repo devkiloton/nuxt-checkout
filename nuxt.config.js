6a0e823076c7bcc69c7924608111b6bc9cf8d5ce
devkiloton <dev.kiloton@gmail.com>
2024-03-05 19:41:30 -0300

    Adding up specific version bootstrap to links

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, {
+      rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css',
+      integrity: 'sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6', crossOrigin: 'anonymous'
+    }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
